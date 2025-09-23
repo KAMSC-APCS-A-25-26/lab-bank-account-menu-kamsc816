1090fdf637bf71a9e64a2ac09c03f9f5dc2457f7
kamsc816 <03samuel.miller@kamsc.org>
2025-09-23 12:53:00 -0400

    Update BankAccountMenu.java

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -11,24 +11,32 @@ public class BankAccountMenu {
         System.out.println("2. Withdraw Money");
         System.out.println("3. Check Balance");
         System.out.println("4. Exit");
-        System.out.println("Intial Balance = " + bAccount);
+        System.out.println("Initial Balance = " + bAccount);
 
-        System.out.println("What do you want to do? ");
+        System.out.println("Enter your choice: ");
         int num = sc.nextInt();
         switch (num) {
             case 1:
-                // kjij
                 System.out.println("How much do you want to add? ");
                 double add = sc.nextDouble();
                 bAccount += add;
+                System.out.println("Added: $" + add);
+                System.out.println("New balance: $" + bAccount);
                 break;
             case 2:
-                System.out.println("How much do you want to withdraw? ");
+                System.out.println("How much do you want to withdraw? $");
                 double sub = sc.nextDouble();
+                System.out.println("Withdrew: $" + sub);
+                if(sub>bAccount)
+                {
+                    System.out.println("Insufficient Funds");
+                    break;
+                }
                 bAccount -= sub;
+                System.out.println("New balance: $");
                 break;
             case 3:
-                System.out.println("Your bank account is at " + bAccount);
+                System.out.println("Your bank account is at $" + bAccount);
                 break;
             case 4:
                 System.out.println("You have exited the program");
